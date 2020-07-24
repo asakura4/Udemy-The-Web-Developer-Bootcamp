d699bef2c9d24f1be760ded77df4810ef0b96114
Tzu-Hao Wang <asakura4@gmail.com>
2020-07-23 22:56:36 -0700

    Add To-Do list practice

---
+++ To-Do List/assets/js/todos.js
@@ -0,0 +1,25 @@
+//Check 
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+
+});
+
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13 && $(this).val().length > 0){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-eraser'></i></span> " + todoText +"</li>");
+
+	}
+});
+
+$(".fa-plus-circle").click(function(){
+	$("input[type='text']").fadeToggle();
+});
